1ae2a3334226c2c108e767af12099f1134f45a58
Leyder Quintero <desarrollador9@wedevel.co>
2024-06-03 17:21:18 -0500

    Documentar restaurants controllers

--- src/controllers/restaurants.ts
+++ src/controllers/restaurants.ts
@@ -4,17 +4,37 @@ import { History, IHistory } from "../models/history";
 
 
 //* Find nearby restaurants
+/**
+ * Find nearby restaurants
+ * @route POST /restaurants
+ * @group Restaurants - Operations about restaurants
+ * @param {User.model} user.body.required - The restaurants data city name or coordinates
+ * @returns {User} 200 - The nearby food places array
+ * @returns {ErrorResponse}  500 - Internal server error response
+ */
 const findRestaurants = async (req: Request, res: Response) => {
     const  userId  = req.userId;
-    const { lon, lat } = req.body.coordinates;
+    const { lon, lat } = req.body?.coordinates || {};
     const city = req.body.city;
 
     try {
-        const url = `${String(process.env['MB_URL_SUGGEST'])}?q=${String(process.env['MB_SEARCH'])}&language=en&proximity=${lon},${lat}&session_token=${String(process.env['MB_SESSION_TOKEN'])}&access_token=${String(process.env['MB_ACCESS_TOKEN'])}`
+        let searchLon = lon;
+        let searchLat = lat;
+
+        if( city ){
+            const forwardUrl = `${String(process.env['MB_URL_FORWARD'])}?q=${city}&access_token=${String(process.env['MB_ACCESS_TOKEN'])}`;
+            const { data: forwardData} = await axios.get( forwardUrl );
+            
+            const { longitude: cityLon, latitude: cityLat } = forwardData.features[0].properties.coordinates;
+            searchLon = cityLon;
+            searchLat = cityLat;
+        }
+        
+        const url = `${String(process.env['MB_URL_SUGGEST'])}?q=${String(process.env['MB_SEARCH'])}&language=es&proximity=${searchLon},${searchLat}&session_token=${String(process.env['MB_SESSION_TOKEN'])}&access_token=${String(process.env['MB_ACCESS_TOKEN'])}`;
 
         const places = await axios.get( url );
-
-        const mapBoxIds = places.data.suggestions.map((el: any) => el.mapbox_id)
+        
+        const mapBoxIds = places.data.suggestions.map((el: any) => el.mapbox_id);
 
         const response = [];
         const history: Omit <IHistory,'id'|'createdAt'|'updatedAt'>[] = [];
@@ -23,6 +43,10 @@ const findRestaurants = async (req: Request, res: Response) => {
             const url = `${String(process.env['MB_URL_RETRIEVE'])}/${id}?session_token=${String(process.env['MB_SESSION_TOKEN'])}&access_token=${String(process.env['MB_ACCESS_TOKEN'])}`
             
             const placeData = await axios.get( url );
+            
+            if( !placeData.data.features.length ){
+                continue;
+            }
 
             const { name, full_address } = placeData.data.features[0].properties;
 
@@ -40,6 +64,15 @@ const findRestaurants = async (req: Request, res: Response) => {
     }
 }
 
+//* Get restaurants history
+/**
+ * Get restaurants history list
+ * @route POST /restaurants/history
+ * @group Restaurants - Operations about restaurants
+ * @param {User.model} user.body.required - The list restaurants history (limit, offset) required
+ * @returns {User} 200 - The restaurants history list
+ * @returns {ErrorResponse}  500 - Internal server error response
+ */
 const getRestaurantsHistory = async (req: Request, res: Response) => {
     const { userId } = req;
     const { limit, offset } = req.body;
