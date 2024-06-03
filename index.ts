1b58f8e5f5bc919b18fb216419884f10fe88ce5f
Leyder Quintero <desarrollador9@wedevel.co>
2024-06-03 09:09:27 -0500

    crear el index.ts

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response, NextFunction } from "express";
+import bodyparser from "body-parser";
+import userRouter from "./src/routes/user";
+import { ServerError } from "./src/util/server-error";
+
+const app = express();
+
+//* Set Middlewares
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+//* Set Headers
+app.use((_, res: Response, next: NextFunction) => {
+    //! Remove cors problem
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
+//* Test Route
+app.get('/', (_: Request, res: Response) => {
+    res.send('Server is running');
+});
+
+//* CRUD Routes
+app.use('/users', userRouter);
+
+//* Error Handling
+app.use((error: ServerError, _: Request, res: Response): void => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message;
+
+    res.status(500).json({ statusCode, message: message });
+});
+
+//* Run server
+app.listen(3000);
